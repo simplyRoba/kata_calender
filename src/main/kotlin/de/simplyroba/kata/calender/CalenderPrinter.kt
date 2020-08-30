f5fa005a6e6c55a2e3f659793dc26247bbcb2ae3
Axel <git@simplyroba.de>
2020-08-30 19:50:03 +0200

    fix start day param

--- src/main/kotlin/de/simplyroba/kata/calender/CalenderPrinter.kt
+++ src/main/kotlin/de/simplyroba/kata/calender/CalenderPrinter.kt
@@ -9,31 +9,31 @@ object CalenderPrinter {
 
     private const val LINE_LENGTH: Int = 20
 
-    fun printMonth(monthToPrint: CalenderMonth, startDay: Weekday = SUNDAY) {
+    fun printMonth(monthToPrint: CalenderMonth, startWeekday: Weekday = SUNDAY) {
         printTitle(monthToPrint)
-        printWeekDayLine(startDay)
-        printDays(monthToPrint.days)
+        printWeekDayLine(startWeekday)
+        printDays(startWeekday, monthToPrint.days)
     }
 
     private fun printTitle(monthToPrint: CalenderMonth) {
         println("%${LINE_LENGTH}s".format("${monthToPrint.month.printName} ${monthToPrint.year}"))
     }
 
-    private fun printWeekDayLine(startDay: Weekday) {
+    private fun printWeekDayLine(startWeekDay: Weekday) {
         for (x in 0..6) {
-            val shiftedIndex = (x.plus(startDay.ordinal) % 7).plus(1)
+            val shiftedIndex = (x.plus(startWeekDay.ordinal) % 7).plus(1)
             print("%2s ".format(Weekday.byIndex(shiftedIndex).shortName))
         }
         println()
     }
 
-    private fun printDays(days: CalenderDays) {
+    private fun printDays(startWeekday: Weekday, days: CalenderDays) {
         val firstDay = days.getValue(1)
 
-        val startingOffset = firstDay.ordinal
+        val startingOffset = (firstDay.ordinal.minus(startWeekday.ordinal) % 7)
         repeat(startingOffset) { print("   ") }
 
-        var counter = firstDay.index
+        var counter = startingOffset.plus(1)
         for (day in days) {
             if (counter%7 == 0) {
                 println("%2d".format(day.key))
---
+++ src/test/kotlin/de/simplyroba/kata/calender/CalenderPrinterTest.kt
@@ -0,0 +1,11 @@
+package de.simplyroba.kata.calender
+
+import org.junit.jupiter.api.Test
+
+/**
+ * @author simplyroba
+ */
+internal class CalenderPrinterTest {
+
+
+}
