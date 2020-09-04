9fb5af3a496cfba325c568fd7fc51d2c5a1f214b
Axel <git@simplyroba.de>
2020-09-04 21:59:51 +0200

    a bit of refactor and more tests

--- src/test/kotlin/de/simplyroba/kata/calender/CalenderMonthTest.kt
+++ src/test/kotlin/de/simplyroba/kata/calender/CalenderMonthTest.kt
@@ -3,6 +3,7 @@ package de.simplyroba.kata.calender
 import com.google.common.truth.Truth.assertThat
 import de.simplyroba.kata.calender.Month.FEBRUARY
 import de.simplyroba.kata.calender.Weekday.MONDAY
+import de.simplyroba.kata.calender.Weekday.SUNDAY
 import org.junit.jupiter.api.Test
 
 /**
@@ -26,7 +27,7 @@ internal class CalenderMonthTest {
 
     @Test
     internal fun `should output weekdays in default order`() {
-        val output = Calender.getMonth(6, 1997).buildOutput().toString()
+        val output = Calender.getMonth(6, 1997).buildOutput(SUNDAY).toString()
         assertThat(output).contains("Su Mo Tu We Th Fr Sa")
     }
 
@@ -36,5 +37,27 @@ internal class CalenderMonthTest {
         assertThat(output).contains("Mo Tu We Th Fr Sa Su")
     }
 
-    // TODO more output tests
+    @Test
+    internal fun `should output days in correct order for FEB 2014`() {
+        val output = Calender.getMonth(2, 2014).buildOutput(SUNDAY).toString()
+        assertThat(output).contains("9 10 11 12 13 14 15")
+        assertThat(output).contains("16 17 18 19 20 21 22")
+        assertThat(output).contains("23 24 25 26 27 28")
+    }
+
+    @Test
+    internal fun `should output days in correct order FEB 2100`() {
+        val output = Calender.getMonth(2, 2100).buildOutput(SUNDAY).toString()
+        assertThat(output).contains("1  2  3  4  5  6")
+        assertThat(output).contains("7  8  9 10 11 12 13")
+        assertThat(output).contains("14 15 16 17 18 19 20")
+        assertThat(output).contains("21 22 23 24 25 26 27")
+    }
+
+    @Test
+    internal fun `should output 29th on FEB 2024`() {
+        val output = Calender.getMonth(2, 2024).buildOutput(SUNDAY).toString()
+        assertThat(output).contains("29")
+        assertThat(output).doesNotContain("30")
+    }
 }
